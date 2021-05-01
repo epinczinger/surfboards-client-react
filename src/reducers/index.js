c4734166f3dab7326ee42c12f3201f9c126b8588
EstebanP <epinczinger@gmail.com>
2021-05-01 17:53:35 -0300

    add surfboard reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import surfboardReducer from './surfboard';
 
 const rootReducer = combineReducers({
-
+  surfboard: surfboardReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
---
+++ src/reducers/surfboard.js
@@ -0,0 +1,12 @@
+const surfboardReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'GET_SURFBOARDS':
+      return action.payload;
+    case 'GET_SURFBOARDS_ERROR':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default surfboardReducer;
