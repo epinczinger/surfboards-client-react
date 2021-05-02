525791f79a3c720242d478c44f9dc3722c42b38e
EstebanP <epinczinger@gmail.com>
2021-05-02 11:27:07 -0300

    add sign out reducer

--- src/reducers/sessions.js
+++ src/reducers/sessions.js
@@ -1,9 +1,14 @@
-const sessionReducer = (state = ['', false, ''], action) => {
+const sessionReducer = (
+  state = ['', false, ''],
+  action,
+) => {
   switch (action.type) {
     case 'RESET_SIGN_ERROR':
       return ['', false, ''];
     case 'SIGN_IN_UP':
       return [action.payload, true, ''];
+    case 'SIGN_OUT':
+      return ['', false, ''];
     case 'SIGN_ERROR':
       return ['', false, action.payload];
     default:
