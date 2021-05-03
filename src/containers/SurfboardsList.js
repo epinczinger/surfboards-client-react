f6c79965eb8e3e6442c05762f10b48835feef6fd
EstebanP <epinczinger@gmail.com>
2021-05-03 17:00:46 -0300

    add surfboardList container

---
+++ src/containers/SurfboardsList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Surfboard from '../components/Surfboard';
+
+const SurfboardsList = () => {
+  const surfboards = useSelector((state) => state.surfboards);
+
+  const surfboardsList = surfboards.map((s) => (
+    <div key={surfboards.id}>
+      <Surfboard
+        id={s.id}
+        model={s.model}
+        brand={s.brand}
+        price={s.price}
+        description={s.description}
+        img={s.image_url}
+      />
+    </div>
+  ));
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      {surfboardsList}
+    </div>
+  );
+};
+
+export default SurfboardsList;
