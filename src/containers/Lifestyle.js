c5b93eab03f7d8d83c7d7891072212d6f5de40f9
EstebanP <epinczinger@gmail.com>
2021-05-03 21:43:50 -0300

    add lifestyle video

---
+++ src/containers/Lifestyle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Lifestyle = () => (
+  <div className="w-screen p-4 shadow">
+    <iframe
+      src="https://www.youtube.com/embed/gpPY-mqGXKs"
+      title="Youtube video"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      width="100%"
+      height="100%"
+    />
+  </div>
+);
+
+export default Lifestyle;
