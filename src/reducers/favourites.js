140654636a9433a79de50c5e327d3c9f1c012e29
EstebanP <epinczinger@gmail.com>
2021-05-07 15:36:34 -0300

    create and delete fav reducer

--- src/reducers/favourites.js
+++ src/reducers/favourites.js
@@ -1,20 +1,14 @@
-/* eslint-disable */
 const favouritesReducer = (state = [], action) => {
   switch (action.type) {
-    case "GET_FAVOURITES":
+    case 'GET_FAVOURITES':
       return action.payload;
-    case "CREATE_FAVOURITE":
+    case 'CREATE_FAVOURITE':
       return [...state, action.payload];
-    case "DELETE_FAVOURITE": {
-
-      const m = state.filter((i) => {
-          // console.log(i.id, action.payload.id, i.kind, action.payload.kind)
-        parseInt(i.id) !== parseInt(action.payload.id) && i.kind !== action.payload.kind
-
-        console.log(m)
-        }  ) 
-        return m}
-    case "FAVOURITES_ERROR":
+    case 'DELETE_FAVOURITE': {
+      const modState = state.filter((p) => parseInt(p.id, 10) !== parseInt(action.payload.id, 10));
+      return modState;
+    }
+    case 'FAVOURITES_ERROR':
       return action.payload;
     default:
       return state;
